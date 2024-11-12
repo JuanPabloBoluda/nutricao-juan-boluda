d72c2dde69c6d014fb4d644c81457cc16fbcf48f
JuanPabloBoluda <164278699+JuanPabloBoluda@users.noreply.github.com>
2024-11-12 00:52:18 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Juan Nutricionista";
+
+var titulo = document.querySelector("h2");
+titulo.textContent = "Pacientes cadastrados";
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
